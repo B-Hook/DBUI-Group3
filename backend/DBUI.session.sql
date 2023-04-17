a485ae5670382a0f429859adf1a218e4ed56da81
beansavvy <savageb@smu.edu>
2023-04-16 21:03:52 -0500

    Updated Surgeon Data

--- backend/DBUI.session.sql
+++ backend/DBUI.session.sql
@@ -13,30 +13,31 @@ CREATE TABLE users (
 );
 
 INSERT INTO users (first_name, last_name, specialty, type, username, password) 
-VALUES  ('John', 'Doe', 'Rheumatology', 'surgeon', 'surgeon1', 'password'),
-        ('Nader', 'Jalili', 'Cardiology', 'surgeon', 'surgeon2', 'password'),
-        ('Gerald', 'Turner', 'Dermatology', 'surgeon', 'surgeon3', 'password'),
+VALUES  ('John', 'Doe', 'Neurological', 'surgeon', 'surgeon1', 'password'),
+        ('Nader', 'Jalili', 'Ophthalmic', 'surgeon', 'surgeon2', 'password'),
+        ('Gerald', 'Turner', 'Spine', 'surgeon', 'surgeon3', 'password'),
         ('Justin', 'Admin', '', 'admin', 'admin', 'password'),
-        ('Jane', 'Doe', 'Rheumatology', 'surgeon', 'surgeon4', 'password'),
-        ('James', 'Smith', 'Cardiology', 'surgeon', 'surgeon5', 'password'),
-        ('David', 'Johnson', 'Dermatology', 'surgeon', 'surgeon6', 'password'),
-        ('Emily', 'Wilson', 'Neurology', 'surgeon', 'surgeon7', 'password'),
-        ('Michael', 'Anderson', 'Ophthalmology', 'surgeon', 'surgeon8', 'password'),
-        ('Rachel', 'Lee', 'Orthopedics', 'surgeon', 'surgeon9', 'password'),
-        ('Robert', 'Wang', 'Plastic Surgery', 'surgeon', 'surgeon10', 'password'),
-        ('Julia', 'Kang', 'Psychiatry', 'surgeon', 'surgeon11', 'password'),
-        ('Daniel', 'Kim', 'Radiology', 'surgeon', 'surgeon12', 'password'),
-        ('Sophia', 'Garcia', 'Urology', 'surgeon', 'surgeon13', 'password'),
-        ('Sarah', 'Davis', 'Allergy and Immunology', 'surgeon', 'surgeon14', 'password'),
-        ('William', 'Lopez', 'Anesthesiology', 'surgeon', 'surgeon15', 'password'),
-        ('Samantha', 'Martinez', 'Endocrinology', 'surgeon', 'surgeon16', 'password'),
-        ('Ava', 'Gonzalez', 'Gastroenterology', 'surgeon', 'surgeon17', 'password'),
-        ('Andrew', 'Perez', 'Hematology', 'surgeon', 'surgeon18', 'password'),
-        ('Ethan', 'Taylor', 'Infectious Disease', 'surgeon', 'surgeon19', 'password'),
-        ('Mia', 'Hernandez', 'Nephrology', 'surgeon', 'surgeon20', 'password'),
+        ('Jane', 'Doe', 'Oncology', 'surgeon', 'surgeon4', 'password'),
+        ('James', 'Smith', 'Trauma Surgery', 'surgeon', 'surgeon5', 'password'),
+        ('David', 'Johnson', 'Obstetrics and Gynecology', 'surgeon', 'surgeon6', 'password'),
+        ('Emily', 'Wilson', 'Neurological', 'surgeon', 'surgeon7', 'password'),
+        ('Michael', 'Anderson', 'Ophthalmic', 'surgeon', 'surgeon8', 'password'),
+        ('Rachel', 'Lee', 'Spine', 'surgeon', 'surgeon9', 'password'),
+        ('Robert', 'Wang', 'Oncology', 'surgeon', 'surgeon10', 'password'),
+        ('Julia', 'Kang', 'Trauma Surgery', 'surgeon', 'surgeon11', 'password'),
+        ('Daniel', 'Kim', 'Obstetrics and Gynecology', 'surgeon', 'surgeon12', 'password'),
+        ('Sophia', 'Garcia', 'Neurological', 'surgeon', 'surgeon13', 'password'),
+        ('Sarah', 'Davis', 'Ophthalmic', 'surgeon', 'surgeon14', 'password'),
+        ('William', 'Lopez', 'Spine', 'surgeon', 'surgeon15', 'password'),
+        ('Samantha', 'Martinez', 'Oncology', 'surgeon', 'surgeon16', 'password'),
+        ('Ava', 'Gonzalez', 'Trauma Surgery', 'surgeon', 'surgeon17', 'password'),
+        ('Andrew', 'Perez', 'Obstetrics and Gynecology', 'surgeon', 'surgeon18', 'password'),
+        ('Ethan', 'Taylor', 'Neurological', 'surgeon', 'surgeon19', 'password'),
+        ('Mia', 'Hernandez', 'Ophthalmic', 'surgeon', 'surgeon20', 'password'),
         ('David', 'Admin', '', 'admin', 'admin2', 'password'),
-        ('Olivia', 'Garcia', 'Oncology', 'surgeon', 'surgeon21', 'password'),
-        ('Emma', 'Rodriguez', 'Pulmonology', 'surgeon', 'surgeon22', 'password');
+        ('Olivia', 'Garcia', 'Spine', 'surgeon', 'surgeon21', 'password'),
+        ('Emma', 'Rodriguez', 'Oncology', 'surgeon', 'surgeon22', 'password');
+
 
 
 CREATE TABLE surgeries(
