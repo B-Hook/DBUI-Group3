c3aeee8ac58f05fae8d77e5b2dd9d7dc24053cc1
beansavvy <savageb@smu.edu>
2023-04-16 20:51:44 -0500

    users dummy data

--- backend/DBUI.session.sql
+++ backend/DBUI.session.sql
@@ -16,7 +16,28 @@ INSERT INTO users (first_name, last_name, specialty, type, username, password)
 VALUES  ('John', 'Doe', 'Rheumatology', 'surgeon', 'surgeon1', 'password'),
         ('Nader', 'Jalili', 'Cardiology', 'surgeon', 'surgeon2', 'password'),
         ('Gerald', 'Turner', 'Dermatology', 'surgeon', 'surgeon3', 'password'),
-        ('Justin', 'Admin', '', 'admin', 'admin', 'password');
+        ('Justin', 'Admin', '', 'admin', 'admin', 'password'),
+        ('Jane', 'Doe', 'Rheumatology', 'surgeon', 'surgeon4', 'password'),
+        ('James', 'Smith', 'Cardiology', 'surgeon', 'surgeon5', 'password'),
+        ('David', 'Johnson', 'Dermatology', 'surgeon', 'surgeon6', 'password'),
+        ('Emily', 'Wilson', 'Neurology', 'surgeon', 'surgeon7', 'password'),
+        ('Michael', 'Anderson', 'Ophthalmology', 'surgeon', 'surgeon8', 'password'),
+        ('Rachel', 'Lee', 'Orthopedics', 'surgeon', 'surgeon9', 'password'),
+        ('Robert', 'Wang', 'Plastic Surgery', 'surgeon', 'surgeon10', 'password'),
+        ('Julia', 'Kang', 'Psychiatry', 'surgeon', 'surgeon11', 'password'),
+        ('Daniel', 'Kim', 'Radiology', 'surgeon', 'surgeon12', 'password'),
+        ('Sophia', 'Garcia', 'Urology', 'surgeon', 'surgeon13', 'password'),
+        ('Sarah', 'Davis', 'Allergy and Immunology', 'surgeon', 'surgeon14', 'password'),
+        ('William', 'Lopez', 'Anesthesiology', 'surgeon', 'surgeon15', 'password'),
+        ('Samantha', 'Martinez', 'Endocrinology', 'surgeon', 'surgeon16', 'password'),
+        ('Ava', 'Gonzalez', 'Gastroenterology', 'surgeon', 'surgeon17', 'password'),
+        ('Andrew', 'Perez', 'Hematology', 'surgeon', 'surgeon18', 'password'),
+        ('Ethan', 'Taylor', 'Infectious Disease', 'surgeon', 'surgeon19', 'password'),
+        ('Mia', 'Hernandez', 'Nephrology', 'surgeon', 'surgeon20', 'password'),
+        ('David', 'Admin', '', 'admin', 'admin2', 'password'),
+        ('Olivia', 'Garcia', 'Oncology', 'surgeon', 'surgeon21', 'password'),
+        ('Emma', 'Rodriguez', 'Pulmonology', 'surgeon', 'surgeon22', 'password');
+
 
 CREATE TABLE surgeries(
     id                      INT AUTO_INCREMENT PRIMARY KEY,
