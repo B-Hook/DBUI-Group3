efe8454b35fa854ed1f8f03069eb3cd69cefa78e
Braiden Hook <bghook@smu.edu>
2023-07-08 01:27:53 -0500

    Update urls, add port

--- frontend/src/Api/LoginApi.js
+++ frontend/src/Api/LoginApi.js
@@ -6,7 +6,7 @@ export const authorizeLogin = async (params) => {
         body: JSON.stringify( params )
     };
 
-    const res = await fetch('https://ec2-3-134-103-222.us-east-2.compute.amazonaws.com/login', req)
+    const res = await fetch('https://ec2-3-134-103-222.us-east-2.compute.amazonaws.com:8000/login', req)
     if (!res.ok) {
         throw new Error(`This is an HTTP error: The status is ${res.status}`);
     }
