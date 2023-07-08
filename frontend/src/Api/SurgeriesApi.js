9870e75bac4e991a327dcaabd8bf205dc901e502
Braiden Hook <bghook@smu.edu>
2023-07-08 01:02:00 -0500

    Updated urls to AWS from localhost

--- frontend/src/Api/SurgeriesApi.js
+++ frontend/src/Api/SurgeriesApi.js
@@ -1,5 +1,5 @@
 export const getSurgeries = async () => {
-    const res = await fetch('http://localhost:8080/surgeries');
+    const res = await fetch('https://ec2-3-134-103-222.us-east-2.compute.amazonaws.com/surgeries');
     if (!res.ok) {
         throw new Error(`This is an HTTP error: The status is ${res.status}`);
     }
@@ -8,7 +8,7 @@ export const getSurgeries = async () => {
 }
 
 export const getSurgeryById = async (id) => {
-    const res = await fetch(`http://localhost:8080/surgeries/${id}`);
+    const res = await fetch(`https://ec2-3-134-103-222.us-east-2.compute.amazonaws.com/surgeries/${id}`);
     if (!res.ok) {
         throw new Error(`This is an HTTP error: The status is ${res.status}`);
     }
@@ -18,7 +18,7 @@ export const getSurgeryById = async (id) => {
 
 export const getSurgeriesBySurgeonId = async (surgeon_id) => {
     console.log(surgeon_id);
-    const res = await fetch(`http://localhost:8080/surgeons/${surgeon_id}/surgeries`);
+    const res = await fetch(`https://ec2-3-134-103-222.us-east-2.compute.amazonaws.com/surgeons/${surgeon_id}/surgeries`);
     if (!res.ok) {
         throw new Error(`This is an HTTP error: The status is ${res.status}`);
     }
@@ -34,7 +34,7 @@ export const createSurgery = async (surgery) => {
         body: JSON.stringify( surgery )
     };
 
-    const res = await fetch('http://localhost:8080/surgeries', req)
+    const res = await fetch('https://ec2-3-134-103-222.us-east-2.compute.amazonaws.com/surgeries', req)
     if (!res.ok) {
         throw new Error(`This is an HTTP error: The status is ${res.status}`);
     }
@@ -51,7 +51,7 @@ export const editSurgery = async (surgery, id) => {
         body: JSON.stringify( surgery )
     };
 
-    const res = await fetch(`http://localhost:8080/surgeries/${id}`, req)
+    const res = await fetch(`https://ec2-3-134-103-222.us-east-2.compute.amazonaws.com/surgeries/${id}`, req)
     if (!res.ok) {
         throw new Error(`This is an HTTP error: The status is ${res.status}`);
     }
@@ -62,7 +62,7 @@ export const editSurgery = async (surgery, id) => {
 
 export const deleteSurgery = async (id) => {
 
-    const res = await fetch(`http://localhost:8080/surgeries/${id}`, {method: 'DELETE'})
+    const res = await fetch(`https://ec2-3-134-103-222.us-east-2.compute.amazonaws.com/surgeries/${id}`, {method: 'DELETE'})
     if (!res.ok) {
         throw new Error(`This is an HTTP error: The status is ${res.status}`);
     }
