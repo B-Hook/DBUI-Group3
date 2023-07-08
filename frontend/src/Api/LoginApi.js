d4ea5b6b97a6ecd7ef073889b6c4f5a0fed9f656
Braiden Hook <bghook@smu.edu>
2023-07-08 01:53:55 -0500

    Updated Apis

--- frontend/src/Api/LoginApi.js
+++ frontend/src/Api/LoginApi.js
@@ -6,7 +6,7 @@ export const authorizeLogin = async (params) => {
         body: JSON.stringify( params )
     };
 
-    const res = await fetch('https://172.18.0.3:8000/login', req)
+    const res = await fetch('https://3.134.103.222:8000/login', req)
     if (!res.ok) {
         throw new Error(`This is an HTTP error: The status is ${res.status}`);
     }
